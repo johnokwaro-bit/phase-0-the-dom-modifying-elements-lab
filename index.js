dddc440e339561b3d6987aadb612458cb8d92bce
John Okwaro <john.okwaro@moringaschool.com>
2022-08-13 15:55:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "<h1#victory>John is the champion</h1>";
+
+body.append(newHeader);
+
+body.append(newHeader)
+
+
