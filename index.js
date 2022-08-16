b1d457ddf2be7df930ecddc3627786f5f9c13932
John Okwaro <john.okwaro@moringaschool.com>
2022-08-16 10:39:20 +0300

    Correction2

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const newHeader = document.createElement("h1");
 
 newHeader.id = "victory";
 
-h1.textContent= "John is the champion";
+newHeader.textContent= "John is the champion";
 
 document.append(newHeader);
